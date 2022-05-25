2170d6e2b01194239faad5e2c5566b35fd9fd47b
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-24 23:55:01 -0400

    updated models part.1

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 
-
-app.listen(PORT, ()=>{
-    console.log('Now listening')
+sequelize.sync({force: false}).then(()=>{
+    app.listen(PORT, ()=>{console.log('Now listening')});
 });
